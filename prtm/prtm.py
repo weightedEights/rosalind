f792fc27b125196b5a68a0dd0d550f5d3ac73923
weightedEights <joshua.a.arndt@gmail.com>
2022-01-09 13:22:08 -0800

    add monoisotopic_mass.table file

--- prtm/prtm.py
+++ prtm/prtm.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 
 """
-Module Docstring
+Rosalind: PRTM
+https://rosalind.info/problems/prtm/
+
+In a weighted alphabet, every symbol is assigned a positive real number called a weight. A string formed from a weighted alphabet is called a weighted string, and its weight is equal to the sum of the weights of its symbols.
+
+The standard weight assigned to each member of the 20-symbol amino acid alphabet is the monoisotopic mass of the corresponding amino acid.
+
+Given: A protein string P of length at most 1000 aa.
+
+Return: The total weight of P. (Consult the monoisotopic mass table.)
+
+Sample Dataset:
+SKADYEK
+
+Sample Output:
+821.392
 """
 
 __author__ = "weightedEights"
