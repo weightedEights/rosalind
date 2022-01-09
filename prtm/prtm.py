005cc1614d77bc280e1def02865207b0fe260a3d
weightedEights <joshua.a.arndt@gmail.com>
2022-01-09 13:29:07 -0800

    updates prtm.py permissions

--- prtm/prtm.py
+++ prtm/prtm.py
@@ -1,12 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 """
 Rosalind: PRTM
 https://rosalind.info/problems/prtm/
 
-In a weighted alphabet, every symbol is assigned a positive real number called a weight. A string formed from a weighted alphabet is called a weighted string, and its weight is equal to the sum of the weights of its symbols.
+In a weighted alphabet, every symbol is assigned a positive real number called a
+weight. A string formed from a weighted alphabet is called a weighted string,
+and its weight is equal to the sum of the weights of its symbols.
 
-The standard weight assigned to each member of the 20-symbol amino acid alphabet is the monoisotopic mass of the corresponding amino acid.
+The standard weight assigned to each member of the 20-symbol amino acid alphabet
+is the monoisotopic mass of the corresponding amino acid.
 
 Given: A protein string P of length at most 1000 aa.
 
@@ -25,7 +28,8 @@ __license__ = "MIT"
 
 
 def main():
-    print(f'Hello, World!')
+    # alphabet as dictionary, key:value as symbol:weight
+    print(f'Hello, Python env!')
 
 
 if __name__ == "__main__":
