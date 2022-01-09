ef4a707d329a2e4062fb72345a9195c015901f92
weightedEights <joshua.a.arndt@gmail.com>
2022-01-09 14:12:17 -0800

    updated prtm with dataset

--- prtm/prtm.py
+++ prtm/prtm.py
@@ -26,9 +26,7 @@ __author__ = "weightedEights"
 __version__ = "0.1"
 __license__ = "MIT"
 
-
 import sys
-
 
 def main():
     # check arguments for proper usage
@@ -36,10 +34,33 @@ def main():
         print(f'Usage: ./prtm.py <path/mass table> <path/dataset>')
         exit(1)
 
+    # read in data set and..
+    dataset = ""
 
     # alphabet as dictionary, key:value as symbol:weight
-    print(f'Name of script: {sys.argv[0]}')
-    print(f'Arg1: {sys.argv[1]}')
+    weighted_alphabet = {}
+
+    with open(sys.argv[2], 'r') as dataset_file, open(sys.argv[1], 'r') as mass_table_file:
+        # handle the dataset string
+        for line in dataset_file:
+            dataset = str(line.strip())
+
+        # handle the mass_table
+        for line in mass_table_file:
+            # print(line.split(' '))
+            # append dictionary, cast value as float for later math
+            weighted_alphabet[line.split(' ')[0]] = float(line.split(' ')[3].strip())
+
+    # work on the dataset
+    # print(f'Alphabet: {weighted_alphabet}')
+    dataset_weight = 0
+
+    # print(f'Dataset: {dataset}')
+    for char in dataset:
+        # print(f'Symbol, Weight: {char}, {weighted_alphabet[char]}')
+        dataset_weight += weighted_alphabet[char]
+
+    print(f'Weight: {dataset_weight}')
 
 
 if __name__ == "__main__":
