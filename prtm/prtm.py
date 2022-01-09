eda0c001410983f411a1867f30db6c8c95aaae7b
weightedEights <joshua.a.arndt@gmail.com>
2022-01-09 13:39:53 -0800

    added sample.dataset

--- prtm/prtm.py
+++ prtm/prtm.py
@@ -27,9 +27,19 @@ __version__ = "0.1"
 __license__ = "MIT"
 
 
+import sys
+
+
 def main():
+    # check arguments for proper usage
+    if len(sys.argv) != 3:
+        print(f'Usage: ./prtm.py <path/mass table> <path/dataset>')
+        exit(1)
+
+
     # alphabet as dictionary, key:value as symbol:weight
-    print(f'Hello, Python env!')
+    print(f'Name of script: {sys.argv[0]}')
+    print(f'Arg1: {sys.argv[1]}')
 
 
 if __name__ == "__main__":
